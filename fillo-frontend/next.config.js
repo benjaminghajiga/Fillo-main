c6c050cef0d489e60a2444fda7d0b91bafa084a8
bghajiga <bghajiga@gmail.com>
2026-01-29 15:34:14 +0000

    go

--- fillo-frontend/next.config.js
+++ fillo-frontend/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api",
+    NEXT_PUBLIC_API_URL: "https://resplendent-creativity-production.up.railway.app/api",
     NEXT_PUBLIC_STACKS_NETWORK: process.env.NEXT_PUBLIC_STACKS_NETWORK || "testnet",
   },
 };
